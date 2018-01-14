5ef5ed26ef391e59d9e5b262bef5f902da1d7bb0
Kevin Cunanan <kevin.a.cunanan@gmail.com>
2018-01-14 01:30:37 -0800

    initialized repo for basic vue frontend of CKNewsletter

---
+++ src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="app">
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="/static/logo.png" id="logo" height="200">
+        </a>
+        <div class="navbar-burger burger" @click="burger = !burger" v-bind:class="{'is-active' : burger}">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" class="navbar-menu" v-bind:class="{'is-active' : burger}">
+        <div class="navbar-start">
+          <a class="navbar-item" href="https://bulma.io/">
+            Home
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="/documentation/overview/start/">
+              Newsletters
+            </a>
+            <div class="navbar-dropdown is-boxed">
+              <a class="navbar-item" href="/documentation/overview/start/">
+                Announcements
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+                Columns
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+                Layout
+              </a>
+              <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+                Form
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                Elements
+              </a>
+              <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                Components
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="button">
+                  <span class="icon">
+                    <i class="fa fa-gift"></i>
+                  </span>
+                  <span>
+                    Donate!
+                  </span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-primary">
+                  <span class="icon">
+                    <i class="fa fa-plus"></i>
+                  </span>
+                  <span>Register</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="contents">
+      <router-view/>
+    </div>
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <img id="footer-logo" src="/static/logo-white.png">
+          <p>And this right here is a spiffy footer, where you can put stuff.</p>
+        </div>
+        <div class="column has-text-right">
+          <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+          <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      burger: false
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import 'mq'
+@import '../node_modules/bulma/bulma.sass'
+
+#app
+  display: flex
+  min-height: 100vh
+  flex-direction: column
+
+.contents
+  flex: 1
+
+.navbar
+  background-color: black
+  .navbar-item, .navbar-link
+    color: $primary
+  .navbar-item img#logo
+    max-height: 2.5rem
+
+.navbar-brand
+  a.navbar-item
+    &:hover
+      background-color: black
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+  #footer-logo
+    height: 150px
+  .icon
+    color: #fff
+    margin-left: 20px
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="home">
+    <section class="hero" style="background-image: url('/static/images/messy_wars.jpg')">
+      <div class="hero-body">
+        <div class="container">
+          <div class="title-container">
+            <h1 class="title">Camp Kesem at the Claremont Colleges</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <h1>Newsletters</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../mq'
+
+.hero
+  background-repeat: no-repeat
+  background-position: center
+  background-size: cover
+  background-color: #808080
+
+.hero-body
+  text-align: center
+
+.title-container
+  +mobile
+    padding: 50px 0
+  +tablet
+    padding: 100px 0
+  +desktop
+    padding: 150px 0
+
+.title
+  color: white
+  +mobile
+    font-weight: bold
+  +tablet
+    font-size: 1.5rem
+  +desktop
+    font-size: 2rem
+    margin-top: 2
+</style>
